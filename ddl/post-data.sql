df19f9c63fab4c81e75f13471deff6e99d58d2da
magnets-moe-dumper <dump@magnets.moe>
2020-12-15 18:09:57 +0000

    Create new dump

---
+++ ddl/post-data.sql
@@ -0,0 +1,241 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 13.1 (Debian 13.1-1.pgdg100+1)
+-- Dumped by pg_dump version 13.1 (Debian 13.1-1.pgdg100+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+--
+-- Name: hash_type hash_type_pkey; Type: CONSTRAINT; Schema: magnets; Owner: postgres
+--
+
+ALTER TABLE ONLY magnets.hash_type
+    ADD CONSTRAINT hash_type_pkey PRIMARY KEY (hash_type);
+
+
+--
+-- Name: rel_torrent_show rel_torrent_show_pkey; Type: CONSTRAINT; Schema: magnets; Owner: postgres
+--
+
+ALTER TABLE ONLY magnets.rel_torrent_show
+    ADD CONSTRAINT rel_torrent_show_pkey PRIMARY KEY (rel_torrent_show_id);
+
+
+--
+-- Name: rel_torrent_show rel_torrent_show_torrent_id_show_id_key; Type: CONSTRAINT; Schema: magnets; Owner: postgres
+--
+
+ALTER TABLE ONLY magnets.rel_torrent_show
+    ADD CONSTRAINT rel_torrent_show_torrent_id_show_id_key UNIQUE (torrent_id, show_id);
+
+
+--
+-- Name: schedule schedule_pkey; Type: CONSTRAINT; Schema: magnets; Owner: postgres
+--
+
+ALTER TABLE ONLY magnets.schedule
+    ADD CONSTRAINT schedule_pkey PRIMARY KEY (schedule_id);
+
+
+--
+-- Name: show show_anilist_id_key; Type: CONSTRAINT; Schema: magnets; Owner: postgres
+--
+
+ALTER TABLE ONLY magnets.show
+    ADD CONSTRAINT show_anilist_id_key UNIQUE (anilist_id);
+
+
+--
+-- Name: show_format show_format_pkey; Type: CONSTRAINT; Schema: magnets; Owner: postgres
+--
+
+ALTER TABLE ONLY magnets.show_format
+    ADD CONSTRAINT show_format_pkey PRIMARY KEY (show_format);
+
+
+--
+-- Name: show_name show_name_pkey; Type: CONSTRAINT; Schema: magnets; Owner: postgres
+--
+
+ALTER TABLE ONLY magnets.show_name
+    ADD CONSTRAINT show_name_pkey PRIMARY KEY (show_name_id);
+
+
+--
+-- Name: show_name_type show_name_type_pkey; Type: CONSTRAINT; Schema: magnets; Owner: postgres
+--
+
+ALTER TABLE ONLY magnets.show_name_type
+    ADD CONSTRAINT show_name_type_pkey PRIMARY KEY (show_name_type);
+
+
+--
+-- Name: show show_pkey; Type: CONSTRAINT; Schema: magnets; Owner: postgres
+--
+
+ALTER TABLE ONLY magnets.show
+    ADD CONSTRAINT show_pkey PRIMARY KEY (show_id);
+
+
+--
+-- Name: state state_pkey; Type: CONSTRAINT; Schema: magnets; Owner: postgres
+--
+
+ALTER TABLE ONLY magnets.state
+    ADD CONSTRAINT state_pkey PRIMARY KEY (key);
+
+
+--
+-- Name: torrent torrent_hash_hash_type_key; Type: CONSTRAINT; Schema: magnets; Owner: postgres
+--
+
+ALTER TABLE ONLY magnets.torrent
+    ADD CONSTRAINT torrent_hash_hash_type_key UNIQUE (hash, hash_type);
+
+
+--
+-- Name: torrent torrent_nyaa_id_key; Type: CONSTRAINT; Schema: magnets; Owner: postgres
+--
+
+ALTER TABLE ONLY magnets.torrent
+    ADD CONSTRAINT torrent_nyaa_id_key UNIQUE (nyaa_id);
+
+
+--
+-- Name: torrent torrent_pkey; Type: CONSTRAINT; Schema: magnets; Owner: postgres
+--
+
+ALTER TABLE ONLY magnets.torrent
+    ADD CONSTRAINT torrent_pkey PRIMARY KEY (torrent_id);
+
+
+--
+-- Name: rel_torrent_show_show_id_nyaa_id_idx; Type: INDEX; Schema: magnets; Owner: postgres
+--
+
+CREATE INDEX rel_torrent_show_show_id_nyaa_id_idx ON magnets.rel_torrent_show USING btree (show_id, nyaa_id DESC);
+
+
+--
+-- Name: show_anilist_id_idx; Type: INDEX; Schema: magnets; Owner: postgres
+--
+
+CREATE INDEX show_anilist_id_idx ON magnets.show USING btree (anilist_id);
+
+
+--
+-- Name: show_name_show_id_idx; Type: INDEX; Schema: magnets; Owner: postgres
+--
+
+CREATE INDEX show_name_show_id_idx ON magnets.show_name USING btree (show_id);
+
+
+--
+-- Name: show_season_idx; Type: INDEX; Schema: magnets; Owner: postgres
+--
+
+CREATE INDEX show_season_idx ON magnets.show USING btree (season);
+
+
+--
+-- Name: torrent_nyaa_id_idx; Type: INDEX; Schema: magnets; Owner: postgres
+--
+
+CREATE INDEX torrent_nyaa_id_idx ON magnets.torrent USING btree (nyaa_id DESC) WHERE matched;
+
+
+--
+-- Name: torrent_nyaa_id_idx1; Type: INDEX; Schema: magnets; Owner: postgres
+--
+
+CREATE INDEX torrent_nyaa_id_idx1 ON magnets.torrent USING btree (nyaa_id DESC) WHERE (NOT matched);
+
+
+--
+-- Name: state on_state_update; Type: TRIGGER; Schema: magnets; Owner: postgres
+--
+
+CREATE TRIGGER on_state_update AFTER UPDATE ON magnets.state FOR EACH ROW EXECUTE FUNCTION magnets.handle_state_update();
+
+
+--
+-- Name: rel_torrent_show rel_torrent_show_nyaa_id_fkey; Type: FK CONSTRAINT; Schema: magnets; Owner: postgres
+--
+
+ALTER TABLE ONLY magnets.rel_torrent_show
+    ADD CONSTRAINT rel_torrent_show_nyaa_id_fkey FOREIGN KEY (nyaa_id) REFERENCES magnets.torrent(nyaa_id);
+
+
+--
+-- Name: rel_torrent_show rel_torrent_show_show_id_fkey; Type: FK CONSTRAINT; Schema: magnets; Owner: postgres
+--
+
+ALTER TABLE ONLY magnets.rel_torrent_show
+    ADD CONSTRAINT rel_torrent_show_show_id_fkey FOREIGN KEY (show_id) REFERENCES magnets.show(show_id);
+
+
+--
+-- Name: rel_torrent_show rel_torrent_show_torrent_id_fkey; Type: FK CONSTRAINT; Schema: magnets; Owner: postgres
+--
+
+ALTER TABLE ONLY magnets.rel_torrent_show
+    ADD CONSTRAINT rel_torrent_show_torrent_id_fkey FOREIGN KEY (torrent_id) REFERENCES magnets.torrent(torrent_id);
+
+
+--
+-- Name: schedule schedule_show_id_fkey; Type: FK CONSTRAINT; Schema: magnets; Owner: postgres
+--
+
+ALTER TABLE ONLY magnets.schedule
+    ADD CONSTRAINT schedule_show_id_fkey FOREIGN KEY (show_id) REFERENCES magnets.show(show_id);
+
+
+--
+-- Name: show_name show_name_show_id_fkey; Type: FK CONSTRAINT; Schema: magnets; Owner: postgres
+--
+
+ALTER TABLE ONLY magnets.show_name
+    ADD CONSTRAINT show_name_show_id_fkey FOREIGN KEY (show_id) REFERENCES magnets.show(show_id);
+
+
+--
+-- Name: show_name show_name_show_name_type_fkey; Type: FK CONSTRAINT; Schema: magnets; Owner: postgres
+--
+
+ALTER TABLE ONLY magnets.show_name
+    ADD CONSTRAINT show_name_show_name_type_fkey FOREIGN KEY (show_name_type) REFERENCES magnets.show_name_type(show_name_type);
+
+
+--
+-- Name: show show_show_format_fkey; Type: FK CONSTRAINT; Schema: magnets; Owner: postgres
+--
+
+ALTER TABLE ONLY magnets.show
+    ADD CONSTRAINT show_show_format_fkey FOREIGN KEY (show_format) REFERENCES magnets.show_format(show_format);
+
+
+--
+-- Name: torrent torrent_hash_type_fkey; Type: FK CONSTRAINT; Schema: magnets; Owner: postgres
+--
+
+ALTER TABLE ONLY magnets.torrent
+    ADD CONSTRAINT torrent_hash_type_fkey FOREIGN KEY (hash_type) REFERENCES magnets.hash_type(hash_type);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
---
+++ ddl/pre-data.sql
@@ -0,0 +1,351 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 13.1 (Debian 13.1-1.pgdg100+1)
+-- Dumped by pg_dump version 13.1 (Debian 13.1-1.pgdg100+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: magnets; Type: SCHEMA; Schema: -; Owner: postgres
+--
+
+CREATE SCHEMA magnets;
+
+
+ALTER SCHEMA magnets OWNER TO postgres;
+
+--
+-- Name: handle_state_update(); Type: FUNCTION; Schema: magnets; Owner: postgres
+--
+
+CREATE FUNCTION magnets.handle_state_update() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+begin
+    if NEW.key = 'max_nyaa_si_id' then
+        if NEW.value::bigint < OLD.value::bigint then
+            call magnets.notify_state_change(NEW.key);
+        end if;
+    elsif NEW.key = 'rematch_unmatched' then
+        if NEW.value::int > 0 then
+            call magnets.notify_state_change(NEW.key);
+        end if;
+    elsif NEW.key in ('last_shows_update', 'last_schedule_update') then
+        if NEW.value::text::timestamptz < OLD.value::text::timestamptz then
+            call magnets.notify_state_change(NEW.key);
+        end if;
+    end if;
+    return null;
+end;
+$$;
+
+
+ALTER FUNCTION magnets.handle_state_update() OWNER TO postgres;
+
+--
+-- Name: notify_state_change(text); Type: PROCEDURE; Schema: magnets; Owner: postgres
+--
+
+CREATE PROCEDURE magnets.notify_state_change(key text)
+    LANGUAGE plpgsql
+    AS $$
+begin
+    perform pg_notify('state_change', key);
+    return;
+end;
+$$;
+
+
+ALTER PROCEDURE magnets.notify_state_change(key text) OWNER TO postgres;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: hash_type; Type: TABLE; Schema: magnets; Owner: postgres
+--
+
+CREATE TABLE magnets.hash_type (
+    hash_type integer NOT NULL,
+    description text NOT NULL,
+    created timestamp with time zone DEFAULT now() NOT NULL
+);
+
+
+ALTER TABLE magnets.hash_type OWNER TO postgres;
+
+--
+-- Name: rel_torrent_show; Type: TABLE; Schema: magnets; Owner: postgres
+--
+
+CREATE TABLE magnets.rel_torrent_show (
+    rel_torrent_show_id bigint NOT NULL,
+    show_id bigint NOT NULL,
+    torrent_id bigint NOT NULL,
+    nyaa_id bigint NOT NULL,
+    created timestamp with time zone DEFAULT now() NOT NULL
+);
+
+
+ALTER TABLE magnets.rel_torrent_show OWNER TO postgres;
+
+--
+-- Name: rel_torrent_show_rel_torrent_show_id_seq; Type: SEQUENCE; Schema: magnets; Owner: postgres
+--
+
+CREATE SEQUENCE magnets.rel_torrent_show_rel_torrent_show_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE magnets.rel_torrent_show_rel_torrent_show_id_seq OWNER TO postgres;
+
+--
+-- Name: rel_torrent_show_rel_torrent_show_id_seq; Type: SEQUENCE OWNED BY; Schema: magnets; Owner: postgres
+--
+
+ALTER SEQUENCE magnets.rel_torrent_show_rel_torrent_show_id_seq OWNED BY magnets.rel_torrent_show.rel_torrent_show_id;
+
+
+--
+-- Name: schedule; Type: TABLE; Schema: magnets; Owner: postgres
+--
+
+CREATE TABLE magnets.schedule (
+    schedule_id bigint NOT NULL,
+    show_id bigint NOT NULL,
+    episode integer NOT NULL,
+    airs_at timestamp with time zone NOT NULL,
+    created timestamp with time zone DEFAULT now() NOT NULL
+);
+
+
+ALTER TABLE magnets.schedule OWNER TO postgres;
+
+--
+-- Name: schedule_schedule_id_seq; Type: SEQUENCE; Schema: magnets; Owner: postgres
+--
+
+CREATE SEQUENCE magnets.schedule_schedule_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE magnets.schedule_schedule_id_seq OWNER TO postgres;
+
+--
+-- Name: schedule_schedule_id_seq; Type: SEQUENCE OWNED BY; Schema: magnets; Owner: postgres
+--
+
+ALTER SEQUENCE magnets.schedule_schedule_id_seq OWNED BY magnets.schedule.schedule_id;
+
+
+--
+-- Name: show; Type: TABLE; Schema: magnets; Owner: postgres
+--
+
+CREATE TABLE magnets.show (
+    show_id bigint NOT NULL,
+    anilist_id bigint NOT NULL,
+    season integer,
+    show_format integer NOT NULL,
+    created timestamp with time zone DEFAULT now() NOT NULL
+);
+
+
+ALTER TABLE magnets.show OWNER TO postgres;
+
+--
+-- Name: show_format; Type: TABLE; Schema: magnets; Owner: postgres
+--
+
+CREATE TABLE magnets.show_format (
+    show_format integer NOT NULL,
+    description text NOT NULL,
+    created timestamp with time zone DEFAULT now() NOT NULL
+);
+
+
+ALTER TABLE magnets.show_format OWNER TO postgres;
+
+--
+-- Name: show_name; Type: TABLE; Schema: magnets; Owner: postgres
+--
+
+CREATE TABLE magnets.show_name (
+    show_name_id bigint NOT NULL,
+    show_id bigint NOT NULL,
+    show_name_type integer NOT NULL,
+    name text NOT NULL,
+    created timestamp with time zone DEFAULT now() NOT NULL
+);
+
+
+ALTER TABLE magnets.show_name OWNER TO postgres;
+
+--
+-- Name: show_name_show_name_id_seq; Type: SEQUENCE; Schema: magnets; Owner: postgres
+--
+
+CREATE SEQUENCE magnets.show_name_show_name_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE magnets.show_name_show_name_id_seq OWNER TO postgres;
+
+--
+-- Name: show_name_show_name_id_seq; Type: SEQUENCE OWNED BY; Schema: magnets; Owner: postgres
+--
+
+ALTER SEQUENCE magnets.show_name_show_name_id_seq OWNED BY magnets.show_name.show_name_id;
+
+
+--
+-- Name: show_name_type; Type: TABLE; Schema: magnets; Owner: postgres
+--
+
+CREATE TABLE magnets.show_name_type (
+    show_name_type integer NOT NULL,
+    description text NOT NULL,
+    created timestamp with time zone DEFAULT now() NOT NULL
+);
+
+
+ALTER TABLE magnets.show_name_type OWNER TO postgres;
+
+--
+-- Name: show_show_id_seq; Type: SEQUENCE; Schema: magnets; Owner: postgres
+--
+
+CREATE SEQUENCE magnets.show_show_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE magnets.show_show_id_seq OWNER TO postgres;
+
+--
+-- Name: show_show_id_seq; Type: SEQUENCE OWNED BY; Schema: magnets; Owner: postgres
+--
+
+ALTER SEQUENCE magnets.show_show_id_seq OWNED BY magnets.show.show_id;
+
+
+--
+-- Name: state; Type: TABLE; Schema: magnets; Owner: postgres
+--
+
+CREATE TABLE magnets.state (
+    key text NOT NULL,
+    value jsonb NOT NULL,
+    created timestamp with time zone DEFAULT now() NOT NULL
+);
+
+
+ALTER TABLE magnets.state OWNER TO postgres;
+
+--
+-- Name: torrent; Type: TABLE; Schema: magnets; Owner: postgres
+--
+
+CREATE TABLE magnets.torrent (
+    torrent_id bigint NOT NULL,
+    nyaa_id bigint NOT NULL,
+    hash bytea NOT NULL,
+    hash_type integer NOT NULL,
+    uploaded_at timestamp with time zone NOT NULL,
+    title text NOT NULL,
+    size bigint NOT NULL,
+    matched boolean DEFAULT false NOT NULL,
+    trusted boolean NOT NULL,
+    created timestamp with time zone DEFAULT now() NOT NULL
+);
+
+
+ALTER TABLE magnets.torrent OWNER TO postgres;
+
+--
+-- Name: torrent_torrent_id_seq; Type: SEQUENCE; Schema: magnets; Owner: postgres
+--
+
+CREATE SEQUENCE magnets.torrent_torrent_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE magnets.torrent_torrent_id_seq OWNER TO postgres;
+
+--
+-- Name: torrent_torrent_id_seq; Type: SEQUENCE OWNED BY; Schema: magnets; Owner: postgres
+--
+
+ALTER SEQUENCE magnets.torrent_torrent_id_seq OWNED BY magnets.torrent.torrent_id;
+
+
+--
+-- Name: rel_torrent_show rel_torrent_show_id; Type: DEFAULT; Schema: magnets; Owner: postgres
+--
+
+ALTER TABLE ONLY magnets.rel_torrent_show ALTER COLUMN rel_torrent_show_id SET DEFAULT nextval('magnets.rel_torrent_show_rel_torrent_show_id_seq'::regclass);
+
+
+--
+-- Name: schedule schedule_id; Type: DEFAULT; Schema: magnets; Owner: postgres
+--
+
+ALTER TABLE ONLY magnets.schedule ALTER COLUMN schedule_id SET DEFAULT nextval('magnets.schedule_schedule_id_seq'::regclass);
+
+
+--
+-- Name: show show_id; Type: DEFAULT; Schema: magnets; Owner: postgres
+--
+
+ALTER TABLE ONLY magnets.show ALTER COLUMN show_id SET DEFAULT nextval('magnets.show_show_id_seq'::regclass);
+
+
+--
+-- Name: show_name show_name_id; Type: DEFAULT; Schema: magnets; Owner: postgres
+--
+
+ALTER TABLE ONLY magnets.show_name ALTER COLUMN show_name_id SET DEFAULT nextval('magnets.show_name_show_name_id_seq'::regclass);
+
+
+--
+-- Name: torrent torrent_id; Type: DEFAULT; Schema: magnets; Owner: postgres
+--
+
+ALTER TABLE ONLY magnets.torrent ALTER COLUMN torrent_id SET DEFAULT nextval('magnets.torrent_torrent_id_seq'::regclass);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
