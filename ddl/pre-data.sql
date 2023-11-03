31ef39d1157f30fce8d48a6074f5e0cd392fd742
magnets-moe-dumper <dump@magnets.moe>
2023-11-03 01:04:20 +0000

    Create new dump

--- ddl/pre-data.sql
+++ ddl/pre-data.sql
@@ -2,8 +2,8 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 13.5 (Debian 13.5-1.pgdg100+1)
--- Dumped by pg_dump version 13.5 (Debian 13.5-1.pgdg100+1)
+-- Dumped from database version 13.12 (Debian 13.12-1.pgdg100+1)
+-- Dumped by pg_dump version 13.12 (Debian 13.12-1.pgdg100+1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
